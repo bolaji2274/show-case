13b6d371a938e03ef96ca8e28d6f462f3ab08a97
bolaji2274 <hammedbolajihammed@gmail.com>
2025-06-15 07:42:28 +0100

    modified src

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./sections/navbar";
-import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Projects from "./sections/Projects";
-import Experiences from "./sections/Experiences";
-import Testimonial from "./sections/Testimonial";
-import Contact from "./sections/Contact";
-import Footer from './sections/Footer';
+import Navbar from "src/sections/navbar";
+import Hero from "src/sections/Hero";
+import About from "src/sections/About";
+import Projects from "src/sections/Projects";
+import Experiences from "src/sections/Experiences";
+import Testimonial from "src/sections/Testimonial";
+import Contact from "src/sections/Contact";
+import Footer from 'src/sections/Footer';
 
 const App = () => {
   return (

8dab390d88cda33e06a866cacc6bf4425f0dafe2
bolaji2274 <hammedbolajihammed@gmail.com>
2026-01-23 22:04:38 +0100

    fix google analytic issues with index .html

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
-import React, { Suspense, useEffect } from "react";
-import ReactGA from "react-ga4";
+import React, { Suspense } from "react";
 
-import Spinner from "./components/Spinner"
+import Spinner from "./components/Spinner";
 import Navbar from "./sections/Navbar";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
 
-// Lazy loaded components
+// Use React.lazy for components you want to load asynchronously
 const Hero = React.lazy(() => import('./sections/Hero'));
 const About = React.lazy(() => import('./sections/About'));
 const Projects = React.lazy(() => import('./sections/Projects'));
 const Experiences = React.lazy(() => import('./sections/Experiences'));
 
-// Fetch the ID from environment variables
-const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_ID;
-
-// Initialize GA only if the ID exists
-if (GA_MEASUREMENT_ID) {
-  ReactGA.initialize(GA_MEASUREMENT_ID);
-}
-
 const App = () => {
-  useEffect(() => {
-    if (GA_MEASUREMENT_ID) {
-      // Send pageview signal
-      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-    } else {
-      console.warn("Google Analytics ID is missing. Check your .env file or build-args.");
-    }
-  }, []);
-
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
